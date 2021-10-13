c4c3a5c82fd3c9f3908139c8ff257966d37cd1c4
Berk Cambaz <berkcambaz12321@gmail.com>
2021-10-13 16:59:05 +0300

    Add mouse controller

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public static Game Instance;
+
+    public Camera cam;
+
     public TilemapManager tilemapManager;
 
     private void Awake()
     {
+        Instance = this;
+
         // Initialize managers
         tilemapManager.Init();
 
---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseController : MonoBehaviour
+{
+    public float zoomMin;
+    public float zoomMax;
+    public float scrollSpeed;
+    private float scroll;
+
+    private Vector3 dragOrigin;
+
+    void Start()
+    {
+        scroll = Game.Instance.cam.orthographicSize;
+    }
+
+    void Update()
+    {
+        // If mouse is over UI, cancel any tile related UI
+        if (EventSystem.current.IsPointerOverGameObject()) return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 offset = Game.Instance.cam.ScreenToWorldPoint(dragOrigin) - Game.Instance.cam.ScreenToWorldPoint(Input.mousePosition);
+            Game.Instance.cam.transform.Translate(offset);
+            dragOrigin = Input.mousePosition;
+        }
+
+        //Vector2Int targetTile = Vector2Int.FloorToInt(Game.Instance.cam.ScreenToWorldPoint(Input.mousePosition));
+        //TileUI.Instance.EnableTileHightlight(targetTile);
+
+        scroll = Mathf.Clamp(scroll - (Input.mouseScrollDelta.y * scrollSpeed), zoomMin, zoomMax);
+        Game.Instance.cam.orthographicSize = Mathf.Lerp(Game.Instance.cam.orthographicSize, scroll, Time.deltaTime * 5f);
+    }
+}
